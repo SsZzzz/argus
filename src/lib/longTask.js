e983767f65ecb3aa77532df999140d9adaf667c0
ze.su <ze.su@tongdun.net>
2023-03-03 15:07:32 +0800

    perf: 去掉longTask的startTime

--- src/lib/longTask.js
+++ src/lib/longTask.js
@@ -5,18 +5,21 @@ import getSelector from '../utils/getSelector';
 function longTask() {
   const observer = new PerformanceObserver((list) => {
     list.getEntries().forEach((entry) => {
-      const lastEvent = getLastEvent();
-      requestIdleCallback(() => {
-        tracker.send({
-          category: 'longTask',
-          eventType: lastEvent?.type,
-          startTime: entry.startTime, // 开始时间
-          duration: entry.duration, // 持续时间
-          selector: lastEvent
-            ? getSelector(lastEvent?.path || lastEvent?.target)
-            : '',
+      // 不加这个条件会有很多的longTask
+      if (entry.duration > 100) {
+        const lastEvent = getLastEvent();
+        requestIdleCallback(() => {
+          tracker.send({
+            category: 'longTask',
+            eventType: lastEvent?.type,
+            // startTime: entry.startTime, // 开始时间
+            duration: entry.duration, // 持续时间
+            selector: lastEvent
+              ? getSelector(lastEvent?.path || lastEvent?.target)
+              : '',
+          });
         });
-      });
+      }
     });
   });
   observer.observe({ entryTypes: ['longtask'] });
